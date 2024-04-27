e5831de6cb9358b3101571ae69633ca47d3274b4
Letícia Carpes <lecarpesoliveira@gmail.com>
2024-04-26 21:38:19 -0300

    atualizando arquivos

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function addUser(){
     let listUser = []
 
     const id = Math.floor(Math.random() * 100);
-    const nome = document.getElementById('name').value;
+    const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const cel = document.getElementById('cel').value;
     const city = document.getElementById('city').value;
@@ -26,7 +26,7 @@ function addUser(){
 
     const objUser = {
         idUser: id,
-        nomeUser: nome,
+        nameUser: name,
         emailUser: email,
         celUser: cel,
         cityUser: city
@@ -80,13 +80,13 @@ function createTableUser(dataUser){
     dataUser.forEach(user => {
         template += `
             <tr>
-                <td> ${user.nomeUser} </td>
+                <td> ${user.nameUser} </td>
                 <td> ${user.emailUser} </td>
                 <td> ${user.celUser} </td>
                 <td> ${user.cityUser} </td>
                 <td>
                         <button type="button" class="button green" onclick="updateUser(${user.idUser})">Editar</button>
-                        <button type="button" class="button red">Excluir</button>
+                        <button type="button" class="button red" onclick="deleteUser(${user.idUser})">Excluir</button>
                 </td>
             </tr>
         `
@@ -111,25 +111,57 @@ function updateUser(id){
 
     const userData = getUserData.find(userId => userId.idUser === id);
 
+    console.log(userData);
 
-    document.getElementById('name').value = userData.nomeUser;
+
+    document.getElementById('name').value = userData.nameUser;
     document.getElementById('email').value = userData.emailUser;
     document.getElementById('cel').value = userData.celUser;
     document.getElementById('city').value = userData.cityUser;
     
-    document.getElementById('saveValues').addEventListener('click', updateUserInfo);
+    document.getElementById('saveValues').addEventListener('click', () => updateUserInfo(id));
 }
 
 
-function updateUserInfo() {
+function updateUserInfo(id) {
+
     const newName = document.getElementById('name').value;
     const newEmail = document.getElementById('email').value;
     const newCel = document.getElementById('cel').value;
     const newCity = document.getElementById('city').value;
 
-    
-    
+    const userList = JSON.parse(localStorage.getItem('client')) || []
+
+    const userIndexFind = userList.findIndex((user) => user.idUser == id)
+
+    if (userIndexFind !== -1) {
+        userList[userIndexFind].nameUser = newName;
+        userList[userIndexFind].emailUser = newEmail;  
+        userList[userIndexFind].celUser = newCel;
+        userList[userIndexFind].cityUser = newCity;
+        
+        localStorage.setItem('client', JSON.stringify(userList));
+    }
+
+    modalClose();
+    window.location.reload();
 }
+
+
+function deleteUser(id){
+    const getUserData = JSON.parse(localStorage.getItem('client'));
+
+    const findUser = getUserData.findIndex((user) => user.idUser == id);
+
+    if (findUser !== -1) {
+        getUserData.splice(findUser, 1);
+
+        localStorage.setItem('client', JSON.stringify(getUserData));
+
+        window.location.reload();
+    }
+}
+
 
 
 
